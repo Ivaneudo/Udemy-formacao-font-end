93413bc9d8ce0a58db4a6103f126407150e1d5b4
Ivaneudo <159584361+Ivaneudo@users.noreply.github.com>
2025-02-02 21:42:34 -0300

    Fim do 2º projeto

--- react_js/PROJETOS-REACT/2_FORM_MULISTEP_REACT/multistep_form_react/components/ReviewForm.jsx
+++ react_js/PROJETOS-REACT/2_FORM_MULISTEP_REACT/multistep_form_react/components/ReviewForm.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import './ReviewForm.css'
 
-const ReviewForm = () => {
+import {
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiFrownFill,
+} from 'react-icons/bs';
+
+
+const ReviewForm = ( {data, updateFieldHandler} ) => {
   return (
-    <div>
-        <h1>ReviewForm</h1>
+    <div className='review-form'>
+        <div className="form-control score-container">
+        <label className='radio-container'>
+              <input 
+              type="radio" 
+              value="very_satisfied" 
+              name='review' 
+              required 
+              checked={data.review === 'unsatisfied'}
+              onChange={(e) => updateFieldHandler('review', e.target.value)}/>
+              <BsFillEmojiFrownFill/>
+              <p>Insatisfeito</p>
+          </label>
+          <label className='radio-container'>
+              <input 
+              type="radio" 
+              value="neutral" 
+              name='review' 
+              required 
+              checked={data.review === 'neutral'}
+              onChange={(e) => updateFieldHandler('review', e.target.value)}/>
+              <BsFillEmojiNeutralFill/>
+              <p>Poderias ser melhor</p>
+          </label>
+          <label className='radio-container'>
+              <input 
+              type="radio" 
+              value="satisfied" 
+              name='review' 
+              required 
+              checked={data.review === 'satisfied'}
+              onChange={(e) => updateFieldHandler('review', e.target.value)}/>
+              <BsFillEmojiSmileFill/>
+              <p>Satisfeito</p>
+          </label>
+          <label className='radio-container'>
+              <input 
+              type="radio" 
+              value="very_satisfield" 
+              name='review' 
+              required 
+              checked={data.review === 'very_satisfield'}
+              onChange={(e) => updateFieldHandler('review', e.target.value)}/>
+              <BsFillEmojiHeartEyesFill/>
+              <p>Muito satisfeito</p>
+          </label>
+        </div>
+        <div className="form-control">
+          <label htmlFor="comment"></label>
+          <textarea 
+            name="comment" 
+            id="comment"
+            placeholder='Conte como foi sua experienciaas   com o produto!'
+            required
+            value={data.comment || ""}
+            onChange={(e) => updateFieldHandler('comment', e.target.value)}
+          >
+          </textarea>
+        </div>
     </div>
   )
 }
--- react_js/PROJETOS-REACT/2_FORM_MULISTEP_REACT/multistep_form_react/src/App.jsx
+++ react_js/PROJETOS-REACT/2_FORM_MULISTEP_REACT/multistep_form_react/src/App.jsx
@@ -1,18 +1,39 @@
 // TODO: Componentes
-import {GrFormNext, GrFormPrevious} from 'react-icons/gr'
-import {FiSend} from 'react-icons/fi'
-import UserForm from '../components/UserForm'
-import ReviewForm from '../components/ReviewForm'
-import Thanks from '../components/Thanks'
+import {GrFormNext, GrFormPrevious} from 'react-icons/gr';
+import {FiSend} from 'react-icons/fi';
+import UserForm from '../components/UserForm';
+import ReviewForm from '../components/ReviewForm';
+import Thanks from '../components/Thanks';
+import Steps from '../components/Steps';
 
 // TODO: Hooks
-import { useForm } from '../hooks/useForm'
+import { useForm } from '../hooks/useForm';
+import {useState} from 'react';
 
-import './App.css'
+import './App.css';
+
+const formTemplate = {
+  name: "",
+  email: "",
+  review: "",
+  comment: "",
+}
 
 function App() {
 
-  const formComponents = [<UserForm/>, <ReviewForm/>, <Thanks/>]
+  const [data, setData] = useState(formTemplate)
+
+  const updateFieldHandler = (key, value) => {
+    setData((prev) => {
+      return {...prev, [key]: value}
+    })
+  }
+
+  const formComponents = [
+    <UserForm data={data} updateFieldHandler={updateFieldHandler}/>, 
+    <ReviewForm data={data} updateFieldHandler={updateFieldHandler}/>, 
+    <Thanks data={data} />
+  ]
 
   const {currestStep, currentComponent, changesStep, isLastStep, isFirstStep} = useForm(formComponents)
 
@@ -26,7 +47,7 @@ function App() {
         </p>
       </div>
       <div className="form-container">
-        <p>Etapas</p>
+        <Steps currestStep={currestStep}/>
         <form onSubmit={(e) => changesStep(currestStep + 1, e)}>
           <div className='inputs-container' >
             {currentComponent}
