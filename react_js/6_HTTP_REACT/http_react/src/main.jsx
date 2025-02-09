175d83404dbe67ae811b1f0992aa6b6201662553
Ivaneudo <159584361+Ivaneudo@users.noreply.github.com>
2025-02-09 19:21:07 -0300

    recomecando

--- react_js/6_HTTP_REACT/http_react/src/main.jsx
+++ react_js/6_HTTP_REACT/http_react/src/main.jsx
@@ -3,8 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { createBrowserRouter, RouterProvider, Route} from 'react-router-dom';
+import Contact from './routes/Contact.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>
+  },
+  {
+    path:'contact',
+    element: <Contact/>
+  }
+])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
