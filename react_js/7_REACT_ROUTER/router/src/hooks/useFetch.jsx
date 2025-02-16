6b7b4529aa37005373ce27e11b2f7188f7039b99
Ivaneudo <159584361+Ivaneudo@users.noreply.github.com>
2025-02-16 19:24:22 -0300

    carregando dados

---
+++ react_js/7_REACT_ROUTER/router/src/hooks/useFetch.jsx
@@ -0,0 +1,86 @@
+import {useState, useEffect} from 'react'
+
+// TODO: 4 custom hook
+export const useFetch = (url) => {
+    const [data, setData] = useState(null);
+
+    // TODO: 5 - refatorando post
+    const [config, setConfig] = useState(null);
+    const [method, setMethod] = useState(null);
+    const [callFetch, setCallFetch] = useState(null);
+
+    // TODO: 6 - loading
+    const [loading, setLoading] = useState(false)
+
+    // TODO: 7 - ERROS
+    const [erros, setErros] = useState(null)
+
+    const httpConfig = (data, method) => {
+        if (method === "post") {
+            setConfig({
+                method,
+                headers: {
+                    "Content-type" : "application/json"
+                },
+                body: JSON.stringify(data)
+            });
+
+            setMethod(method)
+        }
+    }
+
+    useEffect(() => {
+        // TODO: 7 - tratando erros
+        try {
+            
+            const fetchData = async () => {
+    
+                // TODO: 6 - loading
+                setLoading(true)
+    
+                const res = await fetch(url);
+                const json = await res.json();
+    
+                // TODO: 6 - loading
+                setLoading(false)
+    
+                setData(json);
+            }
+
+            fetchData()
+        } catch (error) {
+            console.log(error.message);
+
+            SpeechSynthesisErrorEvent("Ouve algum erro ao carregar os dados!")
+        }
+
+    }, [url, callFetch])
+
+    // TODO: 5 - refatorando post
+    useEffect(() => {
+        const httpRequest = async () => {
+            let json
+
+            if (method === "POST") {
+
+                // TODO: 6 - loading
+                setLoading(true)
+
+                let fetchOptions = [url, config]
+                
+                const res = await fetch(...fetchOptions)
+
+                json = await res.json();
+
+                // TODO: 6 - loading
+                setLoading(false)
+            }
+
+            setCallFetch(json)
+        }
+
+        httpRequest()
+    }, [config, method, url])
+
+    return {data, httpConfig, loading, Error};
+} ;
--- react_js/7_REACT_ROUTER/router/src/routes/Home.jsx
+++ react_js/7_REACT_ROUTER/router/src/routes/Home.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
+// TODO: 6 - Carregamento de dados
+import { useFetch } from '../hooks/useFetch'
+
 const Home = () => {
+
+  const url = 'http://localhost:3000/products';
+
+  const {data: items} = useFetch(url);
+
   return (
     <div>
         <h1>Home</h1>
+        {/* 6 - Carregando dados */}
+        <ul className="porducts">
+          {items && items.map((item) => (
+            <li key={item.id}>
+              <h2>{item.name}</h2>
+              <p>RS: {item.price}</p>
+            </li>
+          ))}
+        </ul>
     </div>
   )
 }
